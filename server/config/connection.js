7eca265347fdbcfeddfa2fad8197708852761454
Deni Mazzei <dconkell@gmail.com>
2022-01-08 11:31:16 -0600

    installed apollo and graphQL

---
+++ server/config/connection.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    process.env.MONGODB_URI || "mongodb://localhost/adventure2learning",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
