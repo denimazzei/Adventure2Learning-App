95fe1b3b52080e9e18a41983986f15f308bdc2f9
Deni Mazzei <dconkell@gmail.com>
2022-01-08 11:02:12 -0600

    added boilerplate placeholders for components views

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from "react";
 import { FaCode } from "react-icons/fa";
 
 function LandingPage() {
-    return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
-    )
+  return (
+    <>
+      <div className="app">
+        <FaCode style={{ fontSize: "4rem" }} />
+        <br />
+        <span style={{ fontSize: "2rem" }}>Adventure 2 Learning</span>
+      </div>
+      <div style={{ float: "right" }}>Copyright 2022</div>
+    </>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
